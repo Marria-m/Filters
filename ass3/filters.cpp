4fccd52be04f7e2afdeaf6de356ccd23e069bc59
Mariam Ehab <160272932+Marria-m@users.noreply.github.com>
2024-03-24 15:26:30 +0200

    Add files via upload
    حطيت الحاجات الي عملتها والي المفروض نضيفها زودوا انتوا بقى هنا الي عملتوه، شوية وهحط كل الصور فملف السي ميك عشان يبقى براحتنا نجرب على كل الصور

---
+++ ass3/filters.cpp
@@ -0,0 +1,61 @@
+#include "Image_Class.h"
+using namespace std;
+
+int main(){
+
+    string file_name;
+    cout << "plz enter name of the image: \n";
+    cin >> file_name;
+    Image img(file_name), new_img;
+
+    new_img(img.width, img.height, 0);
+
+
+    string choice, rotate;
+    cout << "choose what u wanna apply on the Picture\n1)Invert Image\n2)Rotate Image\n3)Adding a Frame to the Picture\n";
+    cin >> choice;
+
+    if (choice == "1"){
+        for (int i = 0; i < img.width; ++i) {
+            for (int j = 0; j < img.height; ++j) {
+
+                img(i, j, 0) = 255 - img(i, j, 0);
+                img(i, j, 1) = 255 - img(i, j, 1);
+                img(i, j, 2) = 255 - img(i, j, 2);
+
+            }
+        }
+    }
+
+    else if (choice == "2"){
+        cout << "rotate the image clockwise by..\n1)90 degree\n2)180 degree\n3)270 degree\n";
+        cin >> rotate;
+        if (rotate == "1"){
+            cout << "haha" << endl;
+            for (int i = 0; i < img.width; ++i) {
+                cout << "jdj" << endl;
+                for (int j = 0; j < img.height; ++j) {
+                    cout << "mama" << endl;
+
+                    img(j, img.width - i - 1, 0) = img(i, j, 0);
+                    img(j, img.width - i - 1, 1) = img(i, j, 1);
+                    img(j, img.width - i - 1, 2) = img(i, j, 2);
+                    cout << i << endl << j << endl;
+
+                }
+
+            }
+        }
+    }
+
+
+    cout << "Pls enter image name to store new image\n";
+    cout << "and specify extension .jpg, .bmp, .png : ";
+
+    cin >> file_name;
+    img.saveImage(file_name);
+
+    cout << "hello world";
+
+    return 0;
+}
